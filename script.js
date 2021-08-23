a18495768821aa843d35678616508ccf7fa341b5
Pavan kalyan <63582758+pavankalyan-5@users.noreply.github.com>
2021-08-23 14:53:45 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(".default_option").click(function(){
-    $(".dropdown ul").addClass("active");
-  });
-  
-  $(".dropdown ul li").click(function(){
-    var text = $(this).text();
-    $(".default_option").text(text);
-    $(".dropdown ul").removeClass("active");
-  });
